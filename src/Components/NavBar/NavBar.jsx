2b661cdbb6b008ff3befb8fef9a52ea2de69e5ff
Reuben <treuben90@yahoo.com>
2023-10-12 16:37:52 +0300

    created the responsiveness for all the pages

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,21 +1,32 @@
+import { useRef } from "react";
 import AvatarProfile from "../AvatarProfile/AvatarProfile";
 import avatar from "../../assets/profile.jpg";
 import SearchBar from "../searchBar/SearchBar";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 import "./NavBar.css";
 
 function NavBar() {
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+    console.log("great");
+  };
   return (
     <div className="navContainer">
       <SearchBar />
       <div className="navAndProfile">
-        <nav>
+        <nav ref={navRef}>
           <ul>
             <Link to="/home">
               <li>Explore</li>
             </Link>
             <li>Create Post</li>
+            <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+              <FaTimes />
+            </button>
           </ul>
         </nav>
         {/* <div className="profile">
@@ -24,6 +35,9 @@ function NavBar() {
         <Link to="/profile">
           <AvatarProfile avatar={avatar} />
         </Link>
+        <button className="nav-btn " onClick={showNavBar}>
+          <FaBars />
+        </button>
       </div>
     </div>
   );
